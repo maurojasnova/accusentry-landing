72f1277dd157215b5526fed159314746674791a1
maurojasnova <andrsnov@gmail.com>
2023-07-03 22:13:57 -0500

    Fixed header popover

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'AccuSentry',
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         {children}
         <Footer />
