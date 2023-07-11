6d01a40fa6785e96fe25a9a2c71d0badfc3da460
maurojasnova <andrsnov@gmail.com>
2023-07-10 20:59:13 -0500

    Modified CTA section. Removed image. Removed irrelevants shadows and lines.

--- app/components/Feature.js
+++ app/components/Feature.js
@@ -58,7 +58,7 @@ export default function Feature() {
                     <img
                         src="/sentrydash.png"
                         alt="SentryDash"
-                        className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+                        className="w-[48rem] max-w-none rounded-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
                         width={2432}
                         height={1442}
                     />
--- app/components/Testimonial.js
+++ app/components/Testimonial.js
@@ -1,8 +1,8 @@
 export default function Testimonial() {
     return (
       <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-        <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
+        <div className="absolute inset-0 -z-10 opacity-20" />
+        <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
         <div className="mx-auto max-w-2xl lg:max-w-4xl">
           <img className="mx-auto h-12" src="/logoClouds/threeM.png" alt="three M Logo" />
           <figure className="mt-10">
@@ -15,9 +15,6 @@ export default function Testimonial() {
               
               <div className="mt-4 flex items-center justify-center space-x-3 text-base">
                 <div className="font-semibold text-gray-900">Patrick Duerr</div>
-                <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                  <circle cx={1} cy={1} r={1} />
-                </svg>
                 <div className="text-gray-600">Advanced Engineering Specialist of 3M</div>
               </div>
             </figcaption>
