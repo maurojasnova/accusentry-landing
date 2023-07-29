f66ead1d11ab5846f41025f9f563042d656cf091
maurojasnova <andrsnov@gmail.com>
2023-07-29 17:15:46 -0500

    Remove popover group from Header and remove dark mode styles from Footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <footer
-  class="bg-white text-center dark:bg-neutral-700 lg:text-left mt-auto">
-  <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
+  class="bg-white text-center lg:text-left mt-auto">
+  <div class="p-4 text-center text-neutral-700">
     © {new Date().getFullYear()} Copyright. AccuSentry, Inc
     
   </div>
